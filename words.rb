0f0ebe63af16426f8620d08c820d5d0c5be21d3c
NimmyVipin <nimmyvipin25@gmail.com>
2016-12-16 18:10:05 +0530

    all combinations of random generated string

--- words.rb
+++ words.rb
@@ -3,10 +3,18 @@
 class Word
 
    def show
-     puts "Enter the  length of random string to be generated"
+    puts "Enter the  length of random string to be generated"
      num=gets.chomp.to_i
-     string=(0...num).map { ('a'..'z').to_a[rand(26)] }.join
-     puts string
+     str=(0...num).map { ('a'..'z').to_a[rand(26)] }
+     print str
+
+     a=Array.new
+     s=(0..num).flat_map{|num| str.to_a.combination(num).map(&:join)}
+     s.each do |p|
+      a.push(p)
+     end 
+    puts a
+
    end
 end
 
